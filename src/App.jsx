7c84c3e57604717025a3f94420be49f67694cef8
Ahamed-ammar <ahamedammar25@gmail.com>
2025-07-04 18:47:48 +0530

    deploy error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React from 'react';
 import "./App.css";
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './components/header/Header';
 import Home from './components/home/home';
 import About from './components/about/about';
@@ -9,6 +9,8 @@ import { Skill } from './components/skills/Skill';
 import { Projects } from './components/projects/projects';
 import { Contact } from './components/contact/contact';
 import Dash from './components/project3-dash/dash';
+
+// Combined full homepage
 const FullHome = () => (
   <>
     <Header />
@@ -24,12 +26,12 @@ const FullHome = () => (
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter basename="/portfolio-react">
       <Routes>
         <Route path="/" element={<FullHome />} />
         <Route path="/dashboard" element={<Dash />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 };
 
