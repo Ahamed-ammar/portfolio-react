6815472d0015a4e2587d986c3baede93d1af212a
Ahamed-ammar <ahamedammar25@gmail.com>
2025-07-04 17:42:07 +0530

    test1

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/portfolio-react/' : '/',
   plugins: [react()],
-  server: {
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-  },
-  base: "/portfolio-react/"
-})
+});
