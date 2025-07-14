d14b5c7aad442e7f63337ce7db924c700cad312a
Ahamed-ammar <ahamedammar25@gmail.com>
2025-07-14 13:12:41 +0530

    Project3 error

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Header } from './components/header/Header';
 import Home from './components/home/home';
 import About from './components/about/about';
 import { Skill } from './components/skills/Skill';
-import { Projects } from './components/projects/projects';
+import { Projects } from './components/projects/Projects';
 import { Contact } from './components/contact/contact';
 import Dash from './components/project3-dash/dash';
 
