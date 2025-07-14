28f0700eb9c985dd3a349c8fca59c44691e89e80
Ahamed-ammar <ahamedammar25@gmail.com>
2025-07-14 19:02:05 +0530

    project 3 check

--- src/components/project3-dash/Dash.jsx
+++ src/components/project3-dash/Dash.jsx
@@ -13,8 +13,8 @@ const Dash = () => {
   return (
     <section className="skill section" id="projects">
       <div style={{ padding: "2rem" }}>
-        <h2 className="section_title">Mini Projects</h2>
-        <span className="section_subtitle">My Projects</span>
+        <h2 className="section_title">Mini Projects.</h2>
+        <span className="section_subtitle">My Projects.</span>
         <div className="dash_container container grid">
           <Rps/>
           <Tc/>
