fcd5757d51e14a2045c7c3882f7ace794b17c60a
Ahamed-ammar <ahamedammar25@gmail.com>
2025-07-14 18:57:59 +0530

    project dirctry

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
+const basename = import.meta.env.MODE === 'production' ? '/portfolio-react' : '/';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolio-react">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
